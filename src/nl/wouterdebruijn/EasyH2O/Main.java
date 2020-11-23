c74280c3a828833851da6490239dba3a3102567c
Luca Don <20096550@student.hhs.nl>
2020-11-23 14:13:23 +0100

    :memo: added the insert and select query.

--- src/nl/wouterdebruijn/EasyH2O/Main.java
+++ src/nl/wouterdebruijn/EasyH2O/Main.java
@@ -1,7 +1,10 @@
 package nl.wouterdebruijn.EasyH2O;
 
 import javax.swing.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -28,10 +31,16 @@ public class Main {
          * Call the connect function
          */
         try {
-            mySQLConnector.connect("localhost", "", "", "");
+            mySQLConnector.connect("localhost", "root", "easy_h2o", "");
 
             // Test code for Connection, Returns false when connection is working.
             System.out.println("Are we closed? : " + mySQLConnector.con.isClosed());
+
+            Boolean a =  printdatabase();
+            Boolean b = sendMicroBitData(microbitData);
+            while (!a || !b){}
+            mySQLConnector.disconnect();
+
         } catch (SQLException throwable) {
             throwable.printStackTrace();
         }
@@ -45,5 +54,40 @@ public class Main {
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
+    }
+
+    public static boolean printdatabase () {
+        try {
+            Statement statement = mySQLConnector.con.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM user;");
+
+            while (resultSet.next()) {
+                String naam = resultSet.getString("naam");
+                String email = resultSet.getString("email");
+                String klantnummer = resultSet.getString("id");
+
+                System.out.println("Klantnummer: " + klantnummer);
+                System.out.println("Naam: " + naam);
+                System.out.println("E-mail: " + email);
+                System.out.println();
+            }
+
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return true;
+    }
+
+    public static boolean sendMicroBitData(String microbitData) {
+        try {
+
+            PreparedStatement preparedStatement = mySQLConnector.con.prepareStatement("INSERT INTO `datapoint`(`regenton`, `data`) VALUES (1, ?)");
+            preparedStatement.setString(1, microbitData);
+            preparedStatement.executeUpdate();
+
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return true;
     }
 }
