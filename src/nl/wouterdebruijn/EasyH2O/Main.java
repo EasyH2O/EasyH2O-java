1300579fd396de69a09d8e3170428ed8f8fcaeb5
Emma van Eesteren <emmavaneesteren@gmail.com>
2020-12-10 14:36:44 +0100

    :sparkles: Added user entity

--- src/nl/wouterdebruijn/EasyH2O/Main.java
+++ src/nl/wouterdebruijn/EasyH2O/Main.java
@@ -2,43 +2,48 @@ package nl.wouterdebruijn.EasyH2O;
 
 import javax.swing.*;
 
-public class Main {
+    public class Main
+    {
 
-    public static MySQLConnector mySQLConnector;
-    public static SerialConnector serialConnector;
+        public static MySQLConnector mySQLConnector;
+        public static SerialConnector serialConnector;
 
-    /**
-     * Main function, creates the Dashboard instance and draws the JFrame.
-     */
-    public static void main(String[] args) {
-        System.out.println("Program started!");
-
-        setUISystemDefault();
-
-        Dashboard dashboard = new Dashboard();
-        dashboard.createAndShow();
-
-        /*
-         * Init MySQL Class.
+        /**
+         * Main function, creates the Dashboard instance and draws the JFrame.
          */
-        mySQLConnector = new MySQLConnector();
-        serialConnector = new SerialConnector();
+        public static void main(String[] args)
+        {
+            System.out.println ("Program started!");
 
-        serialConnector.OpenPort();
-    }
+            setUISystemDefault ();
 
-    /**
-     * Set dashboard theme to system default (Looks nicer!)
-     *
-     * @Author Wouter
-     */
-    public static void setUISystemDefault() {
-        // Set JFrame look and feel to Windows instead of Java.
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            Dashboard dashboard = new Dashboard ();
+            dashboard.createAndShow ();
 
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            /*
+             * Init MySQL Class.
+             */
+            mySQLConnector = new MySQLConnector ();
+            serialConnector = new SerialConnector ();
+
+            serialConnector.OpenPort ();
+        }
+
+        /**
+         * Set dashboard theme to system default (Looks nicer!)
+         *
+         * @Author Wouter
+         */
+        public static void setUISystemDefault()
+        {
+            // Set JFrame look and feel to Windows instead of Java.
+            try
+            {
+                UIManager.setLookAndFeel (UIManager.getSystemLookAndFeelClassName ());
+
+            } catch (Throwable throwable)
+            {
+                throwable.printStackTrace ();
+            }
         }
     }
-}
