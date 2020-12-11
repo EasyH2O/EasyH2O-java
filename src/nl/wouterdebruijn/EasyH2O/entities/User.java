8258c988a30cac3a9ec4847d91870133d07c5891
Wouter de Bruijn <git@electrogamez.nl>
2020-12-11 16:06:54 +0100

    Changed properties to match db class.

--- src/nl/wouterdebruijn/EasyH2O/entities/User.java
+++ src/nl/wouterdebruijn/EasyH2O/entities/User.java
@@ -4,37 +4,38 @@ import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * User class
+ *
  * @author Emma
  */
-public class User
-{
-  private String username;
-  private String hashedPassword;
+public class User {
+    public String id;
+    public String naam;
+    public String email;
+    private final String hashedPassword;
 
-  public User(String username, String hashedPassword)
-  {
-	this.username = username;
-	this.hashedPassword = hashedPassword;
-  }
+    public User(String email, String hashedPassword) {
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+    }
 
-  /**
-   * @author Emma
-   * @param password wordt door user ingevoerd
-   * @return true if password matches hashedPassword else return false
-   */
-  public boolean validatePassword(String password)
-  {
-	return BCrypt.checkpw (password, hashedPassword);
-  }
+    /**
+     * @param password wordt door user ingevoerd
+     * @return true if password matches hashedPassword else return false
+     * @author Emma
+     */
+    public boolean validatePassword(String password) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
 
     /**
      * Create a brand new User (encrypt password)
-     * @param username Username
+     *
+     * @param email     Email of user.
      * @param plainText Plain text password.
      * @return A user instance.
      */
-    public static User newUser(String username, String plainText) {
-        return new User(username, BCrypt.hashpw(plainText, BCrypt.gensalt()));
+    public static User newUser(String email, String plainText) {
+        return new User(email, BCrypt.hashpw(plainText, BCrypt.gensalt()));
     }
 
 }
