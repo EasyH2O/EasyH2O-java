fbee37d10b329d577e89cef16752b5760f14db8d
Wouter de Bruijn <git@electrogamez.nl>
2020-12-11 15:09:50 +0100

    :sparkles: Changed user instance to be created from Hash, added new user function.

--- src/nl/wouterdebruijn/EasyH2O/entities/User.java
+++ src/nl/wouterdebruijn/EasyH2O/entities/User.java
@@ -4,26 +4,36 @@ import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * User class
+ *
  * @author Emma
  */
-public class User
-{
-  private String username;
-  private String hashedPassword;
+public class User {
+  private final String username;
+  private final String hashedPassword;
 
-  public User(String username, String password)
-  {
-	this.username = username;
-	this.hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+  public User(String username, String hashedPassword) {
+    this.username = username;
+    this.hashedPassword = hashedPassword;
   }
 
   /**
-   * @author Emma
    * @param password wordt door user ingevoerd
    * @return true if password matches hashedPassword else return false
+   * @author Emma
    */
-  public boolean validatePassword(String password)
-  {
-	return BCrypt.checkpw (password, hashedPassword);
+  public boolean validatePassword(String password) {
+    return BCrypt.checkpw(password, hashedPassword);
   }
+
+  /**
+   * Create a brand new User (encrypt password)
+   *
+   * @param username  Username
+   * @param plainText Plain text password.
+   * @return A user instance.
+   */
+  public static User newUser(String username, String plainText) {
+    return new User(username, BCrypt.hashpw(plainText, BCrypt.gensalt()));
+  }
+
 }
